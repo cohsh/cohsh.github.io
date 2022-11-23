fe5f6850445e66e5afd32ee30d480b17f189416c
Kohei Ishii <82075327+ko-ishii@users.noreply.github.com>
2022-11-23 16:02:35 +0900

    feat: generate by next.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: process.env.GITHUB_ACTIONS,
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+    return (
+            <div>
+            <h1>Kohei Ishii</h1>
+            <h2>Position</h2>
+            <ul>
+            <li>Ph.D student at The University of Tokyo</li>
+            </ul>
+            <h2>Research Interests</h2>
+            <ul>
+            <li>First-principles calculation</li>
+            <li>Electron-phonon interaction</li>
+            <li>Many-body quantum theory</li>
+            </ul>
+            </div>
+           );
+}
