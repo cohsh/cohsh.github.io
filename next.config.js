1fcb3640f586cc7ece838d6af27b2c3189e3763a
ko-ishii <82075327+ko-ishii@users.noreply.github.com>
2022-12-04 10:42:54 +0900

    delete: all

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: process.env.GITHUB_ACTIONS,
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
