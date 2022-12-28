9e5744eda17b10beba72501327ecd3dc7fc1264f
cohsh <82075327+cohsh@users.noreply.github.com>
2022-12-28 15:46:36 +0900

    [fix] about MathJax

--- src/research.ts
+++ src/research.ts
@@ -43,11 +43,11 @@ function loadResearch() {
         <ul>
         <li>プログラミング [<a href="https://github.com/cohsh">GitHub</a>]
             <ul>
-        <li>Python, JavaScript, Kotlin, PHP, C/C++, Rust, Fortran, Shell</li>
+        <li>Python, JavaScript/TypeScript, Kotlin, PHP, C/C++, Rust, Fortran, Shell</li>
         </ul>
         <li>マークアップ</li>
         <ul>
-        <li>$\\LaTeX$, HTML, Markdown</li>
+        <li>LaTeX, HTML, Markdown</li>
         </ul>
         </ul>
         </div>`
