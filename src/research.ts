ca412d51e8bda8166328f869717ec76a81443508
cohsh <82075327+cohsh@users.noreply.github.com>
2022-12-28 17:35:39 +0900

    [update] research interests

--- src/research.ts
+++ src/research.ts
@@ -9,8 +9,14 @@ function loadResearch() {
         <li>Condensed matter physics</li>
         <ul>
         <li>Electron-phonon interactions</li>
+        <li>Electronic states</li>
+        <li>Phonons</li>
         </ul>
         <li>First-principles calculations</li>
+        <ul>
+        <li>Density functional theory (DFT)</li>
+        <li>Density functional perturbation theory (DFPT)</li>
+        </ul>
         </ul>
         <h3>Tool</h3>
         <ul>
@@ -37,8 +43,14 @@ function loadResearch() {
         <li>物性物理学</li>
         <ul>
         <li>電子・フォノン相互作用</li>
+        <li>電子状態</li>
+        <li>フォノン</li>
         </ul>
         <li>第一原理計算</li>
+        <ul>
+        <li>密度汎関数理論 (DFT)</li>
+        <li>密度汎関数摂動理論 (DFPT)</li>
+        </ul>
         </ul>
         <h3>ツール</h3>
         <ul>
